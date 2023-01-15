f1d51137701a83461970a57261ad1321726fa847
Transcendence <baydindenys@ukr.net>
2023-01-15 22:26:44 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,19 @@
-function fetchCountries(name) {
-fetch('https://restcountries.com/v3.1/name/{name}').then()
+const refs = {
+    searchForm: document.querySelector('#search-box')
+};
 
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch (e) {
+    e.preventDefault();
+
+    fetchCountries(name)
 }
+
+
+
+function fetchCountries(name) {
+return fetch(
+`https://restcountries.com/v3.1/name/${name}`
+).then(response => response.json());
+};
