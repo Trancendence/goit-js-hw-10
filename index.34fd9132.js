231d0a8d2ebab84c66460fcccb41c7740415ad39
Trancendence <109620661+Trancendence@users.noreply.github.com>
2023-01-21 08:47:38 +0000

    Deploying to gh-pages from @ Trancendence/goit-js-hw-10@468ce730236405fc0e3ff84c0a0e283052cb9721 🚀

--- index.34fd9132.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box").addEventListener("submit",(function(e){e.preventDefault(),function(e){fetch("https://restcountries.com/v3.1/name/".concat(e)).then((function(e){return e.json()}))}(name)}));
-//# sourceMappingURL=index.34fd9132.js.map
